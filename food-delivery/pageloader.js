89054c3d7330cb5e387a235d9d8eadc4a3e1b66c
svrtplastics <156522814+SVRTHARDWARE999@users.noreply.github.com>
2025-06-09 21:15:36 +0530

    page loader added

---
+++ food-delivery/pageloader.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+                const loader = document.querySelector('.loader');
+                if (loader) {
+                loader.classList.add('fade-out');
+                }
+            });  
